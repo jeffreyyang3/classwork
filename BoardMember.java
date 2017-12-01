b710b8d7f78c2c2caacffbaf72aa247684a453c2
jeffreyyang3 <33799322+jeffreyyang3@users.noreply.github.com>
2017-12-01 14:02:27 -0800

    Add files via upload

---
+++ BoardMember.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Jeffrey Yang on 12/1/2017.
+ */
+
+public class BoardMember extends Employee{
+    BoardMember(int yearsWorked){
+        super.setYears(yearsWorked);
+    }
+    final double YEARLY_BONUS = 20000; // $/year
+    final int PAID_VACATION = 20; // days
+    final int UNPAID_VACATION = 10; // days
+    final int SICK_LEAVE = 10; // days
+    final double HEALTH_INSURANCE = 20000;//$/year
+    final double INCOME = 200000; // $/year
+
+
+
+
+}
---
+++ Employee.java
@@ -0,0 +1,22 @@
+/* Employee class which will simulate employees of a company.
+ *
+ */
+public class Employee {
+    Employee(){};
+    private int yearsWorked = 0;
+    public Employee(int yearsWorked){
+        this.yearsWorked = yearsWorked;
+    }
+    double YTDValue() {
+        return 0;
+    }
+    int yearsTillRetirement() {
+        return 35 - yearsWorked;
+    }
+    void setYears(int x){
+        yearsWorked = x;
+    }
+    int getYears(){
+        return yearsWorked;
+    }
+}
