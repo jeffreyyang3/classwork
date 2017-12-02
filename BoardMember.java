0d2ac8dbadcb7d2e720f81ade202b2345eb98ce8
JeffreyYang <jeffreyhasmail@gmail.com>
2017-12-01 19:55:26 -0800

    bank

--- BoardMember.java
+++ BoardMember.java
@@ -1,6 +1,6 @@
 /**
  * Created by Jeffrey Yang on 12/1/2017.
- */
+ */ //asdfasdfasdfasdf
 
 public class BoardMember extends Employee{
     BoardMember(int yearsWorked){
@@ -12,6 +12,47 @@ public class BoardMember extends Employee{
     final int SICK_LEAVE = 10; // days
     final double HEALTH_INSURANCE = 20000;//$/year
     final double INCOME = 200000; // $/year
+    int usedUnpaidVacation; // keeps track of how much
+    //unpaid vacation has been used.
+    int usedVacation; // keeps track of how much paid vacation
+    // has been used.
+    int usedSickDays; // keeps track of how many sick days
+    // have been used.
+
+    void usePaidVacation(){
+        PAID_VACATION ++;
+    }
+
+    void useUnpaidVacation(){
+        UNPAID_VACATION++;
+    }
+
+    void useSickDay(){
+        usedSickDays++;
+    } // adds one to usedSickDays
+    void workYear(){
+
+
+
+
+    } // adds one to yearsWorked.
+    double YTDValue(); /* overridden from Employee class.
+    YTDValue() is calculated by taking the sum of:
+    YEARLY_BONUS
+    HEALTH_INSURANCE
+    INCOME
+    PAID_VACATION * (INCOME / 260)
+    (SICK_LEAVE – usedSickDays) * ((INCOME /
+    260) / 2) */
+    int yearsTillRetirement(); /* overridden from Employee
+    class. This should be rounded up to the nearest int. The
+    calculation should take place as floating point arithmetic to ensure
+    precision (hint: cast as double where necessary). This number
+    should not be below zero. This is calculated by: 35 –
+    (yearsWorked + (usedUnpaidVacation / 260) +
+    ((usedVacation / 260) * 2) + (usedSickDays /
+    260))
+    */
 
 
 
