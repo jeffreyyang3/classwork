846d76932b78008f4018a53ca4732b07d9993590
JeffreyYang <jeffreyhasmail@gmail.com>
2017-12-04 20:44:01 -0800

    e3

--- Payroll.java
+++ Payroll.java
@@ -1,5 +1,17 @@
-public class Payroll {
-    Payroll(int x){
+public class Payroll extends Employee {
+    Employee[] employees;
 
+    Payroll(int x){
+        employees = new Employee[x];
+    }
+    void sortEmployeesByRetirement(){
+        for (int i = 0; i < employees.length-1; i++)
+            for (int j = 0; j < employees.length-i-1; j++)
+                if (employees[j].yearsTillRetirement() > employees[j+1].yearsTillRetirement()) {
+                    // swap temp and arr[i]
+                    Employee temp = employees[j];
+                    employees[j] = employees[j+1];
+                    employees[j+1] = temp;
+                }
     }
 }
