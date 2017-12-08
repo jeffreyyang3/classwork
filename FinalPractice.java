ddc07156f81b01810ed47ae0857d4a3972130e84
JeffreyYang <jeffreyhasmail@gmail.com>
2017-12-08 00:52:48 -0800

    pa5

---
+++ FinalPractice.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FinalPractice {
+    public static void main(String []args){
+        //String[] arry = new String[]{"asdf", "gdhdf" , "eqweq","asdf", "dankman", "asdff"};
+        //System.out.println(findIndex(arry, "dankman"));
+        //System.out.println(fib(7));
+
+
+        JFrame frame = new JFrame("test");
+        Container display = frame.getContentPane();
+        JTextField coolman = new JTextField("test2");
+        frame.setSize(500, 500);
+        frame.setLayout(new GridLayout(3,3));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTextField dankman = new JTextField("asdf");
+        frame.add(dankman);
+        frame.add(coolman);
+        dankman.setEditable(false);
+        frame.pack();
+
+        frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    static int findIndex(String[] ar, String s){
+        for(int i = 0; i < ar.length; i++){
+            if(ar[i].equals(s)){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    static int fib(int x){
+        if(x <= 1){
+            return x;
+        }
+        else{
+            return fib(x-1) + fib(x-2);
+        }
+
+
+    }
+    int sum(Node n){
+        if(n == null){
+            return 0;
+        }
+        else{
+            return n.data + sum(n.next);
+        }
+
+    }
+    int iterativesum(Node n){
+        int sum = 0;
+        while(n != null){
+            sum += n.data;
+            n = n.next;
+        }
+        return sum;
+    }
+
+}
