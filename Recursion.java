ee9083424ddd42b2a51aa28964dffdd9c34c92b1
JeffreyYang <jeffreyhasmail@gmail.com>
2018-01-13 00:21:02 -0800

    progress

--- Recursion.java
+++ Recursion.java
@@ -1,40 +1,45 @@
 import java.util.Arrays;
 
 public class Recursion {
-    public static void main(String[] args) {
-        int[] arry = new int[]{2,3,5,1,3,5,69};
-        System.out.println("test1");
-        int max;
-        int[] output = new int[arry.length];
-        //reverseArray3(0, arry.length-1, arry);
-        for (int i = 0; i < arry.length; i++) {
-            System.out.println(arry[i]);
-        }
+    public static void main(String[] args){
+        int[] A = {-1, 2, 6, 12, 9, 2,-5, -2, 8, 5, 7};
+        int[] B = new int[A.length];
+        int[] C = new int[A.length];
+        int minIndex = minArrayIndex(A, 0, A.length - 1);
+        int maxIndex = maxArrayIndex(A, 0, A.length - 1);
+        for(int x: A) System.out.print(x+" ");
         System.out.println();
-        System.out.println(minArrayIndex(arry,0,arry.length-1));
-
-
-
+        System.out.println( "minIndex = " + minIndex );
+        System.out.println( "maxIndex = " + maxIndex );
+        reverseArray1(A, A.length, B);
+        for(int x: B) System.out.print(x+" ");
+        System.out.println();
+        reverseArray2(A, A.length, C);
+        for(int x: C) System.out.print(x+" ");
+        System.out.println();
+        reverseArray3(A, 0, A.length - 1);
+        for(int x: A) System.out.print(x+" ");
+        System.out.println();
     }
 
 
-    private static void reverseArray1(int[] x, int[] y, int n) {
+    private static void reverseArray1(int[] x, int n, int []y) {
 
         if (n != 0) {
             y[n - 1] = x[x.length - n];
-            reverseArray1(x, y, n - 1);
+            reverseArray1(x, n - 1, y);
         }
     }
 
-    private static void reverseArray2(int[] x, int[] y, int n) {
+    private static void reverseArray2(int[] x, int n, int[] y) {
         if (n <= 0) {
             y[x.length - n] = x[n - 1];
-            reverseArray2(x, y, n - 1);
+            reverseArray2(x,n - 1,y);
         }
 
     }
 
-    private static void reverseArray3(int i, int j, int[] x) {
+    private static void reverseArray3(int[] x, int i, int j) {
         if(i >j){
 
         }
@@ -42,7 +47,7 @@ public class Recursion {
             int temp = x[i];
             x[i] = x[j];
             x[j] = temp;
-            reverseArray3(i + 1, j - 1, x);
+            reverseArray3(x, i + 1, j - 1);
         }
     }
 
@@ -81,53 +86,6 @@ public class Recursion {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-        /*int max = 0;
-        int max1 = 0;
-        int maxindex = 0;
-        int maxindex1 = 0;
-        int q = 0;
-
-        if (p == r) {
-            return 0;
-        } else {
-            q = p + (r - p)/2;
-            maxArrayIndex(x, p, q);
-            maxArrayIndex(x, q +1, r);
-        }
-        for(int i = p; i < q; i++){
-
-            if(x[i] > max){
-                max = x[i];
-                maxindex = i;
-            }
-        }
-        for(int j = q; j < r; j++){
-            max = x[j];
-            maxindex1 = j;
-        }
-
-        System.out.println("max left " + maxindex + "max right " + maxindex1);
-
-
-
-        if(x[p] > x[q]){
-            return p;
-        }
-        else{
-            return q;
-        }
-*/
 
 
 
